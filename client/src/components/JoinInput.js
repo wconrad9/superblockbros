4f8c67cc841b5e3a14d5badea0b924044defd041
Walter <wconrad@middlebury.edu>
2020-04-26 14:57:24 -0400

    added functionality to join screen

--- client/src/components/JoinInput.js
+++ client/src/components/JoinInput.js
@@ -6,20 +6,31 @@ const JoinContainer = styled.div`
   margin: 40px;
 `;
 
-const IdInput = styled.input`
+const Input = styled.input`
   display: block;
-  margin: auto;
-  padding: 2.5px;
 `;
 
 const JoinInput = ({ complete }) => {
   const [uniqueId, setUniqueId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const constructPlayer = () => ({
+    uniqueId: uniqueId,
+    username: username
+  });
 
   return (
     <JoinContainer>
-      <IdInput
+      <Input
         type="text"
-        size="100%"
+        size="45"
+        value={username}
+        placeholder="(enter username)"
+        onChange={event => setUsername(event.target.value)}
+      />
+      <Input
+        type="text"
+        size="45"
         value={uniqueId}
         placeholder="Enter a unique id"
         onChange={event => setUniqueId(event.target.value)}
@@ -27,9 +38,9 @@ const JoinInput = ({ complete }) => {
       <div>
         <input
           type="button"
-          disabled={uniqueId === ""}
+          disabled={uniqueId === "" || username === ""}
           onClick={() => {
-            complete(uniqueId);
+            complete(constructPlayer());
           }}
           value="Join Game!"
         />
