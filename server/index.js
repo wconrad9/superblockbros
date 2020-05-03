259fbcc3b8898285dfeff60fc251267cfe7c082d
Walter <wconrad@middlebury.edu>
2020-05-03 12:36:38 -0400

    updated index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { app } = require("./routes");
 const socketIo = require("socket.io");
 
 const server = http.createServer(app).listen(process.env.PORT || 3001);
-console.log("Listening on port %d", server.address().port);
+console.log("Listening on port %d", server.address().port); // eslint-disable-line no-console
 
 const io = socketIo(server);
 
